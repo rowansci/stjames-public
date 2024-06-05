cbede50605bcfb5fdb4e181cbd6c9574fcf2db45
Jonathon Vandezande <jevandezande@gmail.com>
2024-06-05 10:42:27 -0400

    Adds isort and pycodestyle warnings.

--- stjames/calculation.py
+++ stjames/calculation.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
-from .molecule import Molecule
 from .base import Base, LowercaseStrEnum
+from .molecule import Molecule
 from .settings import Settings
 from .status import Status
 
