456bcf5009a2cb6782efb80da4380761edbdebdd
Corin Wagen <corin.wagen@gmail.com>
2024-09-04 10:02:47 -0400

    refactor files a little bit, create periodic cell claß

---
+++ stjames/periodic_cell.py
@@ -0,0 +1,28 @@
+from typing import TypeAlias
+
+import pydantic
+
+from .base import Base
+from .types import Matrix3x3
+
+Bool3: TypeAlias = tuple[bool, bool, bool]
+
+
+class PeriodicCell(Base):
+    lattice_vectors: Matrix3x3
+    is_periodic: Bool3 = (True, True, True)
+
+    @pydantic.field_validator("lattice_vectors")
+    @classmethod
+    def check_tensor_3D(cls, v: Matrix3x3) -> Matrix3x3:
+        if len(v) != 3 or any(len(row) != 3 for row in v):
+            raise ValueError("Cell tensor must be a 3x3 list of floats")
+
+        return v
+
+    @pydantic.field_validator("is_periodic")
+    @classmethod
+    def check_pbc(cls, v: Bool3) -> Bool3:
+        if not any(v):
+            raise ValueError("For periodic boundary conditions, at least one dimension must be periodic!")
+        return v
--- stjames/types.py
+++ stjames/types.py
@@ -4,3 +4,5 @@ UUID: TypeAlias = str
 
 Vector3D: TypeAlias = tuple[float, float, float]
 Vector3DPerAtom: TypeAlias = list[Vector3D]
+
+Matrix3x3: TypeAlias = tuple[Vector3D, Vector3D, Vector3D]
