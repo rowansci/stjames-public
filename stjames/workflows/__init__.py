29cd7d5a51756c7007fd04a83ee89d7e05d401b3
Jonathon Vandezande <jevandezande@gmail.com>
2024-11-07 11:04:26 -0500

    Adds workflow mapping

--- stjames/workflows/__init__.py
+++ stjames/workflows/__init__.py
@@ -1,3 +1,6 @@
+# ruff: noqa: F405
+from typing import Literal
+
 from .admet import *
 from .basic_calculation import *
 from .bde import *
@@ -12,3 +15,37 @@ from .redox_potential import *
 from .scan import *
 from .spin_states import *
 from .tautomer import *
+
+WORKFLOW_NAME = Literal[
+    "admet",
+    "basic_calculation",
+    "bde",
+    "conformers",
+    "conformer_search",
+    "descriptors",
+    "fukui_index",
+    "molecular_dynamics",
+    "multistage_opt",
+    "pka",
+    "redox_potential",
+    "scan",
+    "spin_states",
+    "tautomers",
+]
+
+WORKFLOW_MAPPING = {
+    "admet": ADMETWorkflow,
+    "basic_calculation": BasicCalculationWorkflow,
+    "bde": BDEWorkflow,
+    "conformers": ConformerWorkflow,
+    "conformer_search": ConformerSearchWorkflow,
+    "descriptors": DescriptorsWorkflow,
+    "fukui_index": FukuiIndexWorkflow,
+    "molecular_dynamics": MolecularDynamicsWorkflow,
+    "multistage_opt": MultiStageOptWorkflow,
+    "pka": pKaWorkflow,
+    "redox_potential": RedoxPotentialWorkflow,
+    "scan": ScanWorkflow,
+    "spin_states": SpinStatesWorkflow,
+    "tautomers": TautomerWorkflow,
+}
