26ac4f8f25023dc772ded6659b6b369db62dc309
Spencer Schneider <spencercschneider@gmail.com>
2025-01-17 09:31:28 -0500

    Version bump + ruff format

--- stjames/compute_settings.py
+++ stjames/compute_settings.py
@@ -4,5 +4,7 @@ from .base import Base, LowercaseStrEnum
 class ComputeType(LowercaseStrEnum):
     CPU = "cpu"
     GPU = "gpu"
+
+
 class ComputeSettings(Base):
     compute_type: ComputeType = ComputeType.CPU
