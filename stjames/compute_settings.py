e5110c46d3f4f52b37a4e21ae4b74cddb70aa994
Spencer Schneider <spencercschneider@gmail.com>
2025-01-22 14:59:06 -0500

    Add field for compute type used
    Appease jonathon w | None

--- stjames/compute_settings.py
+++ stjames/compute_settings.py
@@ -4,7 +4,9 @@ from .base import Base, LowercaseStrEnum
 class ComputeType(LowercaseStrEnum):
     CPU = "cpu"
     GPU = "gpu"
+    AUTO = "auto"
 
 
 class ComputeSettings(Base):
-    compute_type: ComputeType = ComputeType.CPU
+    requested_compute_type: ComputeType = ComputeType.AUTO
+    compute_type_used: ComputeType | None = None
