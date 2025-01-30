262576fbac6f1605aa4603cb8edd0d171bdccb11
Spencer Schneider <spencercschneider@gmail.com>
2025-01-30 10:32:36 -0500

    fix fukui

--- stjames/workflows/fukui.py
+++ stjames/workflows/fukui.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from pydantic import AfterValidator
 
 from ..base import round_optional_float
-from ..types import UUID
+from ..types import UUID, FloatPerAtom, round_optional_float_per_atom
 from .workflow import Workflow
 
 
@@ -11,7 +11,7 @@ class FukuiIndexWorkflow(Workflow):
     # UUID of optimization
     optimization: UUID | None = None
 
-    global_electrophilicity_index: float | None = None
-    fukui_positive: Annotated[float | None, AfterValidator(round_optional_float(6))] = None
-    fukui_negative: Annotated[float | None, AfterValidator(round_optional_float(6))] = None
-    fukui_zero: Annotated[float | None, AfterValidator(round_optional_float(6))] = None
+    global_electrophilicity_index: Annotated[float | None, AfterValidator(round_optional_float(6))] = None
+    fukui_positive: Annotated[FloatPerAtom | None, AfterValidator(round_optional_float_per_atom(6))] = None
+    fukui_negative: Annotated[FloatPerAtom | None, AfterValidator(round_optional_float_per_atom(6))] = None
+    fukui_zero: Annotated[FloatPerAtom | None, AfterValidator(round_optional_float_per_atom(6))] = None
