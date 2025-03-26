5541c84de41e882375dd511751b3154e9bb996e0
Corin Wagen <corin.wagen@gmail.com>
2025-03-25 22:24:05 -0400

    fukui settings

--- stjames/workflows/fukui.py
+++ stjames/workflows/fukui.py
@@ -5,6 +5,7 @@ from typing import Annotated
 from pydantic import AfterValidator
 
 from ..base import round_optional_float
+from ..method import Method
 from ..types import UUID, FloatPerAtom, round_optional_float_per_atom
 from .workflow import MoleculeWorkflow
 
@@ -17,6 +18,11 @@ class FukuiIndexWorkflow(MoleculeWorkflow):
     :param initial_molecule: Molecule of interest
     :param mode: Mode for workflow (currently unused)
 
+    Settings:
+    :param do_optimization: whether or not to optimize
+    :param method: the method
+    :param engine_str: the engine
+
     Results:
     :param optimization: UUID of optimization
     :param global_electrophilicity_index: global electrophilicity index
@@ -25,8 +31,11 @@ class FukuiIndexWorkflow(MoleculeWorkflow):
     :param fukui_zero: Fukui index for zero charges
     """
 
-    # UUID of optimization
+    do_optimization: bool = True
     optimization: UUID | None = None
+
+    method: Method = Method.GFN1_XTB
+    engine_str: str | None = None
 
     global_electrophilicity_index: Annotated[float | None, AfterValidator(round_optional_float(6))] = None
     fukui_positive: Annotated[FloatPerAtom | None, AfterValidator(round_optional_float_per_atom(6))] = None
