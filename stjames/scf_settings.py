aa8ee548a09289fe67e1470ecd118af0e407ccef
Corin Wagen <corin.wagen@gmail.com>
2025-08-01 14:30:26 -0400

    3-val logic for soscf

--- stjames/scf_settings.py
+++ stjames/scf_settings.py
@@ -2,6 +2,14 @@ from pydantic import BaseModel
 
 
 class SCFSettings(BaseModel):
-    max_iters: int = 250
+    """
+    Settings for SCF convergence.
 
-    soscf: bool = False
+    :param max_iters: the maximum number of SCF iterations to permit
+    :param soscf: whether or not to use SOSCF (second-order SCF).
+        SOSCF can converge tricky cases, but is also slower than regular SCF algorithms.
+        If `None`, SOSCF will automatically be attempted where regular (DIIS) convergence fails.
+    """
+
+    max_iters: int = 250
+    soscf: bool | None = None
