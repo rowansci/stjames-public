e998b65bcc6a23b429bdc1c1a758ba80fca7c66c
Corin Wagen <corin.wagen@gmail.com>
2025-08-01 14:53:59 -0400

    how's this

--- stjames/scf_settings.py
+++ stjames/scf_settings.py
@@ -1,15 +1,19 @@
-from pydantic import BaseModel
+from .base import Base, LowercaseStrEnum
 
 
-class SCFSettings(BaseModel):
+class UseSOSCF(LowercaseStrEnum):
+    ALWAYS = "always"
+    UPON_FAILURE = "upon_failure"
+    NEVER = "never"
+
+
+class SCFSettings(Base):
     """
     Settings for SCF convergence.
 
     :param max_iters: the maximum number of SCF iterations to permit
     :param soscf: whether or not to use SOSCF (second-order SCF).
-        SOSCF can converge tricky cases, but is also slower than regular SCF algorithms.
-        If `None`, SOSCF will automatically be attempted where regular (DIIS) convergence fails.
     """
 
     max_iters: int = 250
-    soscf: bool | None = None
+    soscf: UseSOSCF = UseSOSCF.UPON_FAILURE
