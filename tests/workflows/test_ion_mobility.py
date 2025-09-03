758d9453dc6ab8a6457e2653fcd59017ee09f042
Corin Wagen <corin.wagen@gmail.com>
2025-09-03 17:32:06 -0400

    add test

---
+++ tests/workflows/test_ion_mobility.py
@@ -0,0 +1,27 @@
+from pytest import fixture, raises
+
+from stjames import Molecule
+from stjames.workflows import IonMobilityWorkflow
+
+
+@fixture
+def water() -> Molecule:
+    return Molecule.from_xyz("H 0 0 0\nO 0 0 1\nH 0 1 1")
+
+
+def test_basic(water: Molecule) -> None:
+    _ = IonMobilityWorkflow(
+        initial_molecule=water,
+        forcefield=[
+            {"name": "oxygen", "atomic_number": 8, "mass": 16.00, "sigma": 0.5, "epsilon": 0.5},
+            {"name": "hydrogen", "atomic_number": 1, "mass": 1.01, "sigma": 0.5, "epsilon": 0.5},
+        ],
+    )
+
+    with raises(ValueError):
+        _ = IonMobilityWorkflow(
+            initial_molecule=water,
+            forcefield=[
+                {"name": "oxygen", "atomic_number": 8, "mass": 16.00, "sigma": 0.5, "epsilon": 0.5},
+            ],
+        )
