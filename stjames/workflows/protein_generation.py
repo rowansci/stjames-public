a40596027620735d596e1850ea45ce4f099fa1f5
Corin Wagen <corin.wagen@gmail.com>
2025-10-20 15:03:33 -0400

    draft of the boltz-gen workflow

---
+++ stjames/workflows/protein_generation.py
@@ -0,0 +1,79 @@
+"""Protein generation workflow."""
+
+from typing import TypeAlias
+
+from ..base import Base, LowercaseStrEnum
+from ..types import UUID
+from .protein_cofolding import AffinityScore, CofoldingScores, Token
+from .workflow import FASTAWorkflow
+
+ProteinUUID: TypeAlias = UUID
+
+
+class ProteinGenerationResult(Base):
+    """The output scores from co-folding scores."""
+
+    sequence: str
+    isolated_structure: ProteinUUID
+    bound_structure: ProteinUUID
+    affinity_score: AffinityScore | None = None
+    scores: CofoldingScores | None = None
+
+
+class BondConstraint(Base):
+    """
+    Specifies bonds between two chains.
+    """
+
+    token1: Token
+    token2: Token
+
+
+class BoltzGenProtocol(LowercaseStrEnum):
+    """
+    The predefined protocol used for generation + filtering.
+    """
+
+    PROTEIN_ANYTHING = "protein-anything"
+    PEPTIDE_ANYTHING = "peptide-anything"
+    PROTEIN_SMALL_MOLECULE = "protein-small_molecule"
+    NANOBODY_ANYTHING = "nanobody-anything"
+
+
+class BoltzGenSettings(Base):
+    """
+    The settings for running BoltzGen.
+
+    :param num_samples: how many designs to generate
+    :param protocol: which protocol to use
+    :param binding_residue: a dict mapping the chain ID to which residues should bind.
+        the string follows the BoltzGen format of specifying ranges of residue indices (refer to their documentation).
+        examples include "5..7,13" or "5..15,50..".
+    """
+
+    protocol: BoltzGenProtocol = BoltzGenProtocol.PROTEIN_ANYTHING
+    num_samples: int = 100
+    binding_residues: dict[int, str] = {}
+
+
+class ProteinCofoldingWorkflow(FASTAWorkflow):
+    """
+    A workflow for generating protein structures.
+
+    Inherited:
+    :param initial_protein_sequences: protein sequences of interest
+    :param initial_smiles_list: SMILES strings of interest
+
+    New:
+    :param input_protein_uuid: the input protein structure, if requested
+    :param protein_generation_settings: the settings for the protein generation method employed
+    :param bond_constraints: BoltzGen bond constraints
+    :param protein_generation_results: the output structures
+    """
+
+    input_protein_uuid: ProteinUUID | None = None
+
+    protein_generation_settings: BoltzGenSettings = BoltzGenSettings()
+    bond_constraints: list[BondConstraint] = []
+
+    protein_generation_results: list[ProteinGenerationResult] = []
