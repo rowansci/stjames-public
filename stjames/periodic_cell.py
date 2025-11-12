dca26f9b9b65ca857f8d2c7472f39dc74c0f19e6
Jonathon Vandezande <jevandezande@gmail.com>
2025-11-12 09:58:56 -0500

    Switches regex to ast.literal_eval

--- stjames/periodic_cell.py
+++ stjames/periodic_cell.py
@@ -1,4 +1,4 @@
-import re
+import ast
 from typing import Annotated, Self, TypeAlias
 
 import numpy as np
@@ -48,20 +48,35 @@ class PeriodicCell(Base):
         """
         Create a PeriodicCell from a string representation.
 
-        >>> PeriodicCell.from_string("[[1, 2, 3], [4, 5, 6], [7, 8, 9]]")
-        PeriodicCell(lattice_vectors=((1.0, 2.0, 3.0), (4.0, 5.0, 6.0), (7.0, 8.0, 9.0)), is_periodic=(True, True, True))
         >>> PeriodicCell.from_string("[(1, -2, 3.0), [4e1, +5, 6], (7.1, 8, 9)]")
         PeriodicCell(lattice_vectors=((1.0, -2.0, 3.0), (40.0, 5.0, 6.0), (7.1, 8.0, 9.0)), is_periodic=(True, True, True))
+        >>> PeriodicCell.from_string("[(a, -2, 3.0), [4e1, +5, 6], (7.1, 8, 9)]")
+        Traceback (most recent call last):
+        ...
+        ValueError: Could not parse cell from string: [(a, -2, 3.0), [4e1, +5, 6], (7.1, 8, 9)]
+        >>> PeriodicCell.from_string("[(1, -2, 3.0), [4e1, +5, 6], (7.1, 8, 9, 10)]")
+        Traceback (most recent call last):
+        ...
+        ValueError: Cell must be a 3x3 matrix: got [(1, -2, 3.0), [4e1, +5, 6], (7.1, 8, 9, 10)]
+        >>> PeriodicCell.from_string("[('a', -2, 3.0), [4e1, +5, 6], (7.1, 8, 9)]")
+        Traceback (most recent call last):
+        ValueError: Cell must be a 3x3 matrix of numbers: got [('a', -2, 3.0), [4e1, +5, 6], (7.1, 8, 9)]
+
         """
-        if match := re.match(_cell_pattern, string.strip()):
-            cell = [[float(num) for num in line.split(",")] for line in match.groups()]
-        else:
-            raise ValueError(f"String '{string}' is not a valid representation of a PeriodicCell.")
+        try:
+            cell = ast.literal_eval(string)
+        except (ValueError, SyntaxError) as e:
+            raise ValueError(f"Could not parse cell from string: {string}") from e
+
+        if not isinstance(cell, (list, tuple)):
+            raise ValueError(f"Cell must be a list or tuple: got {string}")
+        if not len(cell) == 3:
+            raise ValueError(f"Cell must be a 3x3 matrix: got {string}")
+        if not all(isinstance(row, (list, tuple)) for row in cell):
+            raise ValueError(f"Cell must be a 3x3 matrix: got {string}")
+        if not all(len(row) == 3 for row in cell):
+            raise ValueError(f"Cell must be a 3x3 matrix: got {string}")
+        if not all(all(isinstance(x, (int, float)) for x in row) for row in cell):
+            raise ValueError(f"Cell must be a 3x3 matrix of numbers: got {string}")
 
         return cls(lattice_vectors=cell)
-
-
-_ob = r"\s*[\[\(]\s*"
-_cb = r"\s*[\]\)]\s*"
-_line = rf"\s*{_ob}(.+?){_cb}\s*"
-_cell_pattern = f"{_ob}{_line},{_line},{_line}{_cb}"
