dc1c7331883f9583098abb4a6ac97e9f5bc99598
Jonathon Vandezande <jevandezande@gmail.com>
2025-11-18 11:00:58 -0500

    Test engine settings

--- tests/workflows/test_basic_calculation.py
+++ tests/workflows/test_basic_calculation.py
@@ -13,3 +13,28 @@ def test_basic_calculation() -> None:
 
     assert workflow.engine == Engine.XTB
     assert workflow.calculation_uuid is None
+
+
+def test_engine_setting() -> None:
+    """Test the deprecated Engine setting."""
+    mol = Molecule.from_xyz("He 0 0 0")
+
+    workflow = BasicCalculationWorkflow(
+        initial_molecule=mol,
+        engine=Engine.PYSCF,
+        settings={"method": "PBE"},
+    )
+    assert workflow.engine == Engine.PYSCF
+
+    workflow = BasicCalculationWorkflow(
+        initial_molecule=mol,
+        settings={"method": "PBE", "engine": "pyscf"},
+    )
+    assert workflow.engine == Engine.PYSCF
+
+    workflow = BasicCalculationWorkflow(
+        initial_molecule=mol,
+        engine=Engine.GPU4PYSCF,
+        settings={"method": "PBE", "engine": "pyscf"},
+    )
+    assert workflow.engine == Engine.GPU4PYSCF
