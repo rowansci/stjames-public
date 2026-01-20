ac23b97272aca735db9d400100a4c600c162576d
Corin Wagen <corin.wagen@gmail.com>
2026-01-20 11:46:08 -0500

    test moment

--- tests/workflows/test_pose_analysis_md.py
+++ tests/workflows/test_pose_analysis_md.py
@@ -20,6 +20,3 @@ def test_raises(water: Molecule, gfp: str) -> None:
     PoseAnalysisMolecularDynamicsWorkflow(protein=gfp, initial_smiles="O", mode=Mode.RAPID)
 
     PoseAnalysisMolecularDynamicsWorkflow(protein=gfp, initial_smiles="O", mode=Mode.RAPID, protein_restraint_cutoff=5.0, protein_prune_cutoff=7.0)
-
-    with raises(ValueError):
-        PoseAnalysisMolecularDynamicsWorkflow(protein=gfp, initial_smiles="O", mode=Mode.RAPID, protein_restraint_cutoff=5.0, protein_prune_cutoff=3.0)
